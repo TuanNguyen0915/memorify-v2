7a192188c8bb058dc71c55f350aed924b43957e0
Tuan Nguyen <tuan.nat915@gmail.com>
2024-02-20 04:55:40 -0500

    fixed Leftsidebar

--- components/LeftSidebar/LeftSidebar.jsx
+++ components/LeftSidebar/LeftSidebar.jsx
@@ -1,46 +1,55 @@
-"use client";
+// "use client";
 import UserInfo from "@/components/LeftSidebar/UserInfo";
 import UserManagement from "@/components/LeftSidebar/UserManagement";
 import NavLinks from "@/components/LeftSidebar/NavLinks";
 import { SignedIn, useUser } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
-import { Spinner } from "../Spinner/Spiner";
+// import { useEffect, useState } from "react";
+import { Spinner } from "../Spinner/Spinner";
 
-const LeftSidebar = () => {
-  const [loading, setLoading] = useState(false);
-  const { user, isLoaded } = useUser();
-  const [userData, setUserData] = useState(null);
+const LeftSidebar = ({ user }) => {
+  // const [loading, setLoading] = useState(false);
+  // const { user, isLoaded } = useUser();
+  // const [userData, setUserData] = useState(null);
 
-  const getUser = async () => {
-    try {
-      setLoading(true);
-      const res = await fetch(`api/user/${user?.id}`);
-      const data = await res.json();
-      setUserData(data);
-      setLoading(false);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  useEffect(() => {
-    if (user) {
-      getUser();
-    }
-  }, [user]);
+  // const getUser = async () => {
+  //   try {
+  //     setLoading(true);
+  //     const res = await fetch(`api/user/${user?.id}`);
+  //     const data = await res.json();
+  //     setUserData(data);
+  //     setLoading(false);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+  // useEffect(() => {
+  //   if (user) {
+  //     getUser();
+  //   }
+  // }, [user]);
 
   return (
+    
+    // <div className="sidebar flexBetween flex-col gap-10 px-4">
+    //   {loading || !isLoaded ? (
+    //     <Spinner />
+    //   ) : (
+    //     userData && (
+    //       <SignedIn>
+    //         <UserInfo user={userData} />
+    //         <NavLinks />
+    //         <UserManagement />
+    //       </SignedIn>
+    //     )
+    //   )}
+    // </div>
+
     <div className="sidebar flexBetween flex-col gap-10 px-4">
-      {loading || !isLoaded ? (
-        <Spinner />
-      ) : (
-        userData && (
-          <SignedIn>
-            <UserInfo user={userData} />
-            <NavLinks />
-            <UserManagement />
-          </SignedIn>
-        )
-      )}
+      <SignedIn>
+        <UserInfo user={user} />
+        <NavLinks />
+        <UserManagement />
+      </SignedIn>
     </div>
   );
 };
--- components/LeftSidebar/UserInfo.jsx
+++ components/LeftSidebar/UserInfo.jsx
@@ -1,45 +1,57 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+"use client"
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-
-const UserInfo = ({user}) => {
+const UserInfo = ({ user }) => {
+  const router = useRouter()
   return (
-    <div className='flex flex-col w-full gap-10'>
-      <div className='flex flex-col items-center w-full gap-8'>
-        <Link href='/' className='w-full text-5xl text-center group'>
-          <p className='font-bold duration-300 text-secondary-100 group-hover:text-textColor-100'>
-            Merori<span className='font-bold duration-300 text-textColor-100 group-hover:text-secondary-100'>fy</span>
-          </p>
-        </Link>
-      </div>
-      <div className='flex-col w-full gap-2 flexCenter'>
-        <div className='relative rounded-full w-[70px] h-[70px]'>
-        <Image
-          src={user.profilePhoto}
-          alt='user avatar'
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          className='object-cover object-center rounded-full hover:shadow-secondary-100 hover:shadow-2xl'
-        />
-        </div>
-        <p className='w-full text-xl text-center'>{user.firstName} {user.lastName}</p>
-      </div>
-      <div className='w-full flexBetween'>
-        <div className='w-full text-center'>
-          <p className='text-lg font-bold'>{user.posts.length}</p>
-          <p>Posts</p>
-        </div>
-        <div className='w-full text-center'>
-          <p className='text-lg font-bold'>{user.followers.length}</p>
-          <p>Followers</p>
-        </div>
-        <div className='w-full text-center'>
-          <p className='text-lg font-bold'>{user.followings.length}</p>
-          <p>Following</p>
-        </div>
-      </div>
+    <div className="flex w-full flex-col gap-10">
+      {user && (
+        <>
+          <div className="flex w-full flex-col items-center gap-8">
+            <Link href="/" className="group w-full text-center text-5xl">
+              <p className="font-bold text-secondary-100 duration-300 group-hover:text-textColor-100">
+                Merori
+                <span className="font-bold text-textColor-100 duration-300 group-hover:text-secondary-100">
+                  fy
+                </span>
+              </p>
+            </Link>
+          </div>
+          <div className="flexCenter w-full flex-col gap-2">
+            <div className="relative h-[70px] w-[70px] rounded-full"
+            onClick={()=>{router.push(`/profile/${user._id}`)}}>
+              <Image
+                src={user.profilePhoto}
+                alt="user avatar"
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                className="rounded-full object-cover object-center hover:shadow-2xl hover:shadow-secondary-100"
+              />
+            </div>
+            <p className="w-full text-center text-xl">
+              {user.firstName} {user.lastName}
+            </p>
+          </div>
+          <div className="flexBetween w-full">
+            <div className="w-full text-center">
+              <p className="text-lg font-bold">{user.posts?.length}</p>
+              <p>Posts</p>
+            </div>
+            <div className="w-full text-center">
+              <p className="text-lg font-bold">{user.followers?.length}</p>
+              <p>Followers</p>
+            </div>
+            <div className="w-full text-center">
+              <p className="text-lg font-bold">{user.followings?.length}</p>
+              <p>Following</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
-export default UserInfo
+  );
+};
+export default UserInfo;
